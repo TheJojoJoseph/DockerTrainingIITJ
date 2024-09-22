6cfb3825fcb48e4fff8cfa253905cca772518292
Jojo Joseph <jojojoseph11@gmail.com>
2024-09-22 21:38:44 +0530

    Updating the terraforms

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -1,44 +1,39 @@
 # Configure the Google Cloud provider
 provider "google" {
-  credentials = file("service-account-key.json")
-  project     = var.project_id  # Use the project ID from variables.tf
+  credentials = file("./service-account-key.json")  
+  project     = var.project_id  # Make sure var.project_id is defined in your variables.tf
   region      = "us-central1"   # Specify your region
 }
 
-# Create a firewall rule to allow public access to ports 8000 and 5001
-resource "google_compute_firewall" "allow_public_ports" {
-  name    = "allow-public-ports"
-  network = "default"
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8000", "5001"]
+# Create a health check for the instance group
+resource "google_compute_health_check" "default" {
+  name = "docker-training-health-check"
+  http_health_check {
+    port         = 80
+    request_path = "/"
   }
-
-  source_ranges = ["0.0.0.0/0"]
 }
 
 # Create an instance template
 resource "google_compute_instance_template" "template" {
-  name = "docker-training-template"
-
+  name         = "docker-training-template"
   machine_type = "f1-micro"  # 0.5 GB RAM
   region       = "us-central1"
 
   disk {
-    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"  # Using Ubuntu 20.04 LTS
+    source_image = "ubuntu-os-cloud/ubuntu-2004-lts"
     auto_delete  = true
   }
 
   network_interface {
     network = "default"
-    access_config {
+    access_config {  # This block assigns a public IP
+      // You can optionally set `nat_ip` here for a specific static IP.
     }
   }
 
   metadata_startup_script = <<-EOT
     #!/bin/bash
-    # Install required packages
     sudo apt-get update
     sudo apt-get install -y docker.io docker-compose git
 
@@ -56,43 +51,37 @@ resource "google_compute_instance_template" "template" {
 # Create a managed instance group
 resource "google_compute_region_instance_group_manager" "ig_manager" {
   name               = "docker-training-ig"
-  version            = {
-    instance_template = google_compute_instance_template.template.self_link
-  }
   base_instance_name = "docker-training-instance"
   target_size        = 1  # Initial instance count
 
-  # Set auto-scaling
-  autoscaling {
-    min_replicas = 1
-    max_replicas = 5
-    target_cpu_utilization {
-      target_utilization = 0.6
-    }
+  version {
+    instance_template = google_compute_instance_template.template.self_link
+  }
+
+  named_port {
+    name = "http"
+    port = 80
   }
 
   region = "us-central1"
-}
 
-# Create a health check
-resource "google_compute_health_check" "default" {
-  name = "docker-training-health-check"
-  http_health_check {
-    port = 80
-    request_path = "/"
+  auto_healing_policies {
+    health_check      = google_compute_health_check.default.self_link
+    initial_delay_sec = 300
   }
 }
 
-# Attach the health check to the instance group
+# Create an autoscaler for the instance group
 resource "google_compute_region_autoscaler" "autoscaler" {
-  name                = "docker-training-autoscaler"
-  region              = "us-central1"
-  target              = google_compute_region_instance_group_manager.ig_manager.self_link
+  name   = "docker-training-autoscaler"
+  region = "us-central1"
+  target = google_compute_region_instance_group_manager.ig_manager.self_link
+
   autoscaling_policy {
     min_replicas = 1
     max_replicas = 5
     cpu_utilization {
-      target_utilization = 0.6
+      target = 0.6  # Target CPU utilization percentage
     }
   }
 }
--- Terraform/outputs.tf
+++
@@ -1,4 +0,0 @@
-# Output the instance group URL
-output "instance_group_url" {
-  value = google_compute_region_instance_group_manager.ig_manager.self_link
-}
